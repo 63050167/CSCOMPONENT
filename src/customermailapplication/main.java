9216fafc9c4f8294c3db17952e3a4e5ebfc15684
Tontun <63050167@kmitl.ac.th>
2023-07-21 10:26:11 +0700

    first commit

---
+++ src/customermailapplication/main.java
@@ -0,0 +1,8 @@
+package src.customermailapplication;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.print("What to draw? 1. Ellipse, 2. Rectangle");
+        
+    }
+}
